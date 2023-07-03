ccbb813b13c3dea9f065025a53d28af63b119e41
Nate Harada <nharada1@gmail.com>
2023-07-03 14:26:31 -0700

    Fix a bug with binary classifiers

--- scripts/integration_test.py
+++ scripts/integration_test.py
@@ -1,10 +1,43 @@
 import argparse
+import os
+from io import BytesIO
 from typing import Any
 
 import numpy as np
+import requests
 from PIL import Image
+from sklearn.linear_model import LogisticRegression  # type: ignore
 
 from zeroshot import Classifier, create_preprocess_fn
+
+
+def url_to_image(url):
+    response = requests.get(url)
+    image = Image.open(BytesIO(response.content))
+    return image
+
+
+def golden_test(target: str, classifier: Any) -> np.ndarray:
+    sklearn_model = LogisticRegression()
+    sklearn_model.coef_ = classifier.linear_model.coefs
+    sklearn_model.intercept_ = classifier.linear_model.intercept
+    sklearn_model.classes_ = np.arange(len(classifier.classes))
+
+    # Load the image into a numpy array.
+    if target.startswith("http"):
+        image = url_to_image(target)
+    else:
+        image = Image.open(target)
+    image_np = np.array(image)[:, :, 0:3]
+
+    # Preprocess the image using the classifier.
+    image_np = classifier.preprocess_fn(image_np)
+
+    # Extract the features from the image using the classifier.
+    features = classifier.feature_extractor.process(image_np)
+
+    # Run the classifier.
+    return sklearn_model.predict_proba(features)
 
 
 def run_test(target: str, classifier: Any) -> None:
@@ -25,26 +58,40 @@ def run_test(target: str, classifier: Any) -> None:
         if prediction_str == class_name:
             print("\033[0m", end="")
 
-    # Check the results.
+    # Check the results using sklearn.
+    expected_probs = golden_test(target, classifier).ravel()
     assert prediction_str == "a photo of a zoo animal"
-    for expected, prob in zip(
-        [0.05371324, 0.03506953, 0.04463777, 0.86657947], prediction_probs
-    ):
+    for expected, prob in zip(expected_probs, prediction_probs):
+        assert np.isclose(expected, prob, atol=1e-5)
+
+
+def run_test_binary(target: str, classifier: Any) -> None:
+    # Run the classifier.
+    prediction = classifier.predict(target)
+    prediction_probs = classifier.predict_proba(target)
+
+    # Run the sklearn version
+    comparison = golden_test(target, classifier).ravel()
+
+    # Check the results.
+    for expected, prob in zip(comparison, prediction_probs):
         assert np.isclose(expected, prob, atol=1e-5)
 
 
 def main(args: argparse.Namespace) -> None:
-    classifier = Classifier(args.model)
-    run_test(args.image, classifier)
+    classifier = Classifier(os.path.join(args.test_file_path, "test_model.json"))
+    run_test(os.path.join(args.test_file_path, "giraffe.png"), classifier)
     run_test(
         "https://moonshine-assets.s3.us-west-2.amazonaws.com/giraffe.png", classifier
     )
 
+    classifier = Classifier(os.path.join(args.test_file_path, "test_binary.json"))
+    run_test_binary(os.path.join(args.test_file_path, "giraffe.png"), classifier)
+
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
-    args.add_argument("--model", default="scripts/test_files/test_model.json", type=str)
-    args.add_argument("--image", default="scripts/test_files/giraffe.png", type=str)
+    args.add_argument("--test_file_path", default="scripts/test_files", type=str)
     known_args = args.parse_args()
 
     main(known_args)
--- zeroshot/logistic_regression.py
+++ zeroshot/logistic_regression.py
@@ -1,9 +1,13 @@
 import numpy as np
 
 
-def softmax(X):
+def softmax(X: np.ndarray) -> np.ndarray:
     exp_X = np.exp(X - np.max(X, axis=1, keepdims=True))
     return exp_X / np.sum(exp_X, axis=1, keepdims=True)
+
+
+def expit(X: np.ndarray) -> np.ndarray:
+    return 1 / (1 + np.exp(-X))
 
 
 class LogisticRegression(object):
@@ -11,12 +15,16 @@ class LogisticRegression(object):
         self.coefs = coefs
         self.intercept = intercept
 
-    def predict_proba(self, X):
+    def predict_proba(self, X: np.ndarray) -> np.ndarray:
+        # For a binary classifier, we need to add the negative of the coefficients.
         z = np.dot(X, self.coefs.T) + self.intercept
-        probabilities = softmax(z)
-        return probabilities
+        if self.coefs.shape[0] == 1:
+            prob = expit(z)
+            return np.vstack([1 - prob, prob]).T
+        else:
+            return softmax(z)
 
-    def predict(self, X):
+    def predict(self, X: np.ndarray) -> np.ndarray:
         probabilities = self.predict_proba(X)
         predictions = np.argmax(probabilities, axis=1)
         return predictions
